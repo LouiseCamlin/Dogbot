921fb345a0db11d054868a427fb30a1cb97faaba
Louise Camlin <lacamlin@gmail.com>
2017-12-03 20:07:18 +0000

    update breeds beginning with A

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,4 +10,23 @@ Dog.create({breed: "Akita.", fact: " Large breed from the mountainous northern r
 
 Dog.create({breed: "Alaskan Malamute.", fact: " Originally bred for hauling heavy freight because of their strength and endurance, and later breed as a sled dog. "})
 
+Dog.create({breed: "American English Coonhound.", message: " Descended from English Foxhounds brought to America in 17th/18th centuries. Known for speed and endurance."})
+
+Dog.create({breed: "American Eskimo Dog.", message: " Companion dog originating from Germany and member of the Spitz family."})
+
+Dog.create({breed: "American Foxhound.", message: " Cousin of the English Foxhound, they are bred to hunt foxes by scent."})
+
+Dog.create({breed: "American Pit Bull Terrier.", message: " Created by breeding Old English Terriers and Old English Bulldogs together it is medium-sized, and has a short coat and smooth well-defined muscle structure."})
+
+Dog.create({breed: "American Water Spaniel.", message: " Breed of spaniel which was developed in the state of Wisconsin during the 19th century, their coat is water resistant."})
+
+Dog.create({breed: "Anatolian Shepherd Dog.", message: " Originated in the Anatolia region of central Turkey. Bred to protect livestock in hot dry summers and very cold winters."})
+
+Dog.create({breed: "Appenzeller Sennenhunde.", message: " One of 4 regional breeds of Sennenhund type dogs from the Swiss Alps"})
+
+Dog.create({breed: "Austrailian Cattle Dog.", message: " A breed of herding dog originally developed in Australia for droving cattle over long distances across rough terrain."})
+
+Dog.create({breed:"Austrailian Shepherd.", message: " Despite its name it was developed on ranches in the Western United States during the 19th century."})
+
+Dog.create({breed: "Azawakh.", message: " A sighthound and cattle guardian breed from West Africa. They can hunt gazelle and hare at speeds up to 40 miles per hour."})
 
