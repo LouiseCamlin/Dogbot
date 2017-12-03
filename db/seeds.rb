febf746a39c8322cde24ce621217828f1cc8a17c
Louise Camlin <lacamlin@gmail.com>
2017-12-03 19:17:30 +0000

    add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Dog.delete_all()
+
+Dog.create({breed:"Affenpinscher.", message: " A terrier-like toy breed. German in origin dating back to the 17th century. Name derived from the German word Affe meaning ape or monkey, due to their monkey like facial features."})
+
+Dog.create({breed:"Afghan Hound.", message: " Distinguished by a long, silky coat and tail with ring curl. Selectively bred for its unique features in the cold mountains of Afghanistan."})
+
+Dog.create({breed: "Airedale Terrier.", message: " Originated in the valley (dale) of the River Aire. Traditionally called King of Terriers as it's the largest terrier breed."})
+
+Dog.create({breed: "Akita.", message: " Large message from the mountainous northern regions of Japan. Originally used for guarding royalty and nobility in feudal Japan. "})
+
+Dog.create({breed: "Alaskan Malamute.", message: " Originally bred for hauling heavy freight because of their strength and endurance, and later breed as a sled dog. "})
+
+
